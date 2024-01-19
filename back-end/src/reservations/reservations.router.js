0cf90c73bc08b8820428556aae23e50e92a4e67b
Nirbhay Giri <33128137+girinirbhay@users.noreply.github.com>
2024-01-19 22:25:46 +0530

    Update reservations.router.js

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,22 +1,16 @@
 const router = require("express").Router();
 const controller = require("./reservations.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router
-    .route("/:reservationId/status")
-    .put(controller.updateStatus)
-    .all(methodNotAllowed);
+router.route("/")
+  .get(controller.list)
+  .post(controller.create);
 
-router
-    .route("/:reservationId")
-    .get(controller.read)
-    .put(controller.updateReservation)
-    .all(methodNotAllowed);
+router.route("/:reservationId")
+  .get(controller.read)
+  .put(controller.update);
 
-router
-    .route("/")
-    .get(controller.list)
-    .post(controller.create)
-    .all(methodNotAllowed);
+router.route("/:reservationId/status")
+  .put(controller.updateStatus);
+
 
 module.exports = router;
